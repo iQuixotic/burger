87ed4d5e99130a23a5580be7ddc981a39db8e7da
Trey <treyw1850@gmail.com>
2018-05-08 08:52:37 -0400

    changed connection for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,30 @@ const mysql = require("mysql");
 
 // my connection set up. the 3306 port might need to be changed when I export maybe...
 // this should set up everything from the router to connect with mySQL
-const connection = mysql.createConnection({
-  port: 3306,
+
+// const connection = mysql.createConnection({
+//   port: 3306,
+//   host: "localhost",
+//   user: "root",
+//   password: "root",
+//   database: "burger_db"
+// });
+
+
+
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+
+ connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "root",
   database: "burger_db"
 });
+};
+
+
 
 connection.connect(function(err) {
   if (err) {
